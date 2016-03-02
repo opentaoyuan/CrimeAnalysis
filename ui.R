bca22d09a208e3c2b55924994cff254b5858ff4c
shadowkshs <fxm900206216@gmail.com>
2016-03-02 17:31:44 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,55 +1,52 @@
 shinyUI(fluidPage(
-  titlePanel("Taoyuan Crimes"),
-  h3("Data:104/11~105/1", style = "color:red"),
+  titlePanel("桃園竊盜分析"),
+  h3("Datarange:104/11~105/1", style = "color:red"),
   downloadButton('downloadData', 'Download'),
   br(),
   span("last update:105/2/5", style = "color:blue"),
     mainPanel(
       tabsetPanel(type = "tabs",
-                  tabPanel("MAP",
+                  tabPanel("竊盜熱點",
                            fluidRow(     
                             column(3,
-                              selectInput("map_type", label = h4("Type"),c("All","Motorcycle theft","Residential burglary","Car theft")),
-                              selectInput("map_breau", label = h4("Breau"),
-                                         c("All","Taoyuan","Zhongli","Yangmei","Dayaun","Daxi","Pingzhen","Bade","Guishan","Luzhu","Longtan")),                                   
-                              selectInput("map_year", label = h4("Year"),c(104,105)),      
-                              sliderInput("map_month", label = h4("Month"),min = 1, max = 12, value = 11),
-                              sliderInput("map_zoom", label = h4("Zoom"),min = 11, max = 14, value = 11),
-                              span("Design by DSP", style = "color:darkorange"),
-                              img(src = "DSP.png", height = 100, width = 100)
+                              selectInput("map_type", label = h4("竊盜種類"),c("All","機車竊盜","住宅竊盜","汽車竊盜")),
+                              selectInput("map_breau", label = h4("分局"),
+                                         c("All","桃園分局","中壢分局","楊梅分局","大園分局","大溪分局","平鎮分局","八德分局","龜山分局","蘆竹分局","龍潭分局")),                                   
+                              selectInput("map_year", label = h4("年月"),c(10411,10412,10501)),      
+                              sliderInput("map_zoom", label = h4("縮放"),min = 11, max = 14, value = 11),
+                              span("Design by TYPD", style = "color:darkorange"),
+                              img(src = "TYPD.png", height = 100, width = 100)
                                   ),
                             column(6,
-                              h3("Density maps"),
+                              h3("竊盜密度圖"),
                               plotOutput("map",width = "800px",height = "800px")
                                   )
                            )),#map_panel
-                  tabPanel("Analysis_breau",
+                  tabPanel("轄區分析",
                            fluidRow(
                              column(3,
-                                    selectInput("anb_year", label = h4("Year"),c(104,105)),      
-                                    sliderInput("anb_month", label = h4("Month"),min = 1, max = 12, value = 11),
-                                    span("Design by DSP", style = "color:darkorange"),
-                                    img(src = "DSP.png", height = 100, width = 100)
+                                    selectInput("anb_year", label = h4("年度"),c(10411,10412,10501)),      
+                                    span("Design by TYPD", style = "color:darkorange"),
+                                    img(src = "TYPD.png", height = 100, width = 100)
                              ),
                                column(3,
-                                   h3("Bars by breau"),
+                                   h3("轄區竊盜件數堆疊直方圖"),
                                    ggvisOutput("breau_plot")
                                    ) 
                            )
                   ),#Analysis_breau_panel
-                    tabPanel("Data",
+                    tabPanel("資料檢視",
                            fluidRow(
                              column(3,
-                                    selectInput("data_type", label = h4("Type"),c("All","Motorcycle theft","Residential burglary","Car theft")),
-                                    selectInput("data_breau", label = h4("Breau"),
-                                                c("All","Taoyuan","Zhongli","Yangmei","Dayaun","Daxi","Pingzhen","Bade","Guishan","Luzhu","Longtan")),
-                                    selectInput("data_year", label = h4("Year"),c(104,105)),      
-                                    sliderInput("data_month", label = h4("Month"),min = 1, max = 12, value = 11),
-                                    span("Design by DSP", style = "color:darkorange"),
-                                    img(src = "DSP.png", height = 100, width = 100)
+                                    selectInput("data_type", label = h4("竊盜種類"),c("All","機車竊盜","住宅竊盜","汽車竊盜")),
+                                    selectInput("data_breau", label = h4("分局"),
+                                                c("All","桃園分局","中壢分局","楊梅分局","大園分局","大溪分局","平鎮分局","八德分局","龜山分局","蘆竹分局","龍潭分局")),
+                                    selectInput("data_year", label = h4("年度"),c(10411,10412,10501)),      
+                                    span("Design by TYPD", style = "color:darkorange"),
+                                    img(src = "TYPD.png", height = 100, width = 100)
                              ),
                              column(6,
-                                    h3("Table"),
+                                    h3("資料檢視"),
                                     tableOutput("Data")     
                              )
                            )
